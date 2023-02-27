afb3d50c598df903474d95c72f9a010defd0b0dd
Spoogh <joshuafloresjjf@gmail.com>
2023-02-27 10:38:30 -0600

    header added

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+
+import {images} from '../../constants'
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='app__aboutus app__bg flex__center section__padding' id="about">
+    <div className='app__aboutus-overlay flex__center'>
+      <img src={images.G} alt="G letter"/>
+    </div>
   </div>
 );
 
