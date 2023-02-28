638f3f8d1f069459dc66407860805033e2ae3266
Spoogh <joshuafloresjjf@gmail.com>
2023-02-27 21:29:07 -0600

    started awards section

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 
+import {SubHeading} from '../../components';
+import {images, data} from '../../constants';
+
 import './Laurels.css';
 
+const AwardCard = ({award:{ imgUrl, title, subtitle}})=>(
+   <div className='app__laurels_awards-card'>
+    <img src={imgUrl} alt="award" />
+   </div>
+)
+
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className='app__bg app__wrapper section__padding' id="rewards">
+    <div className='app__wrapper_info'>
+      <SubHeading title="Awards & Recognitions"/>
+      <h1 className='headtext__cormorant'>Our Laurels</h1>
+         
+         <div className='app__laurels_awards'>
+            {data.awards.map((award)=> <AwardCard award={award} key={award.title}/>)}
+         </div>
+
+      </div>
+
+      <div className='app__wrapper_img'>
+          
+      </div>
   </div>
 );
 
